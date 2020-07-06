db4edf7e273b8865892db37c4afbe27bd229e8b5
admiswalker <admiswalker@gmail.com>
2020-07-06 23:54:38 +0900

    rm unused files

--- main_ex1.cpp
+++ main_ex1.cpp
@@ -7,20 +7,22 @@ int main(){
     auto alc = std::allocator<int>();
 
     // 10要素のint型が入る領域を確保
-    int* arr = alc.allocate(10);
+    int* pArr = alc.allocate(10);
 
     // 確保した領域の各要素を構築する(コンストラクタを呼び出す)
-    for (std::size_t i = 0; i != 10; ++i) alc.construct(arr + i);
-
-    std::iota(arr, arr + 10, 0);
-    std::for_each(arr, arr + 10, [](int i) { std::cout << i << " "; });
+    for(std::size_t i=0; i<10; ++i){
+        alc.construct(&pArr[i]);
+    }
+    
+    std::iota(&pArr[0], &pArr[10], 0);
+    std::for_each(&pArr[0], &pArr[10], [](int i){ std::cout << i << " "; });
     std::cout << std::endl;
 
     // 配列の各要素を破棄する(デストラクタを呼び出す)
-    for (std::size_t i = 0; i != 10; ++i) alc.destroy(arr + i);
+    for(std::size_t i=0; i<10; ++i){ alc.destroy(&pArr[i]); }
 
     // 領域を解放する
-    alc.deallocate(arr, 10);
+    alc.deallocate(pArr, 10);
 
     return 0;
 }
