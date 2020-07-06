dea7e323fc43bdfa5c222cfdb9fe320dbf40d3a5
admiswalker <admiswalker@gmail.com>
2020-07-07 00:40:47 +0900

    add ex3 (udi)

---
+++ main_ex3.cpp
@@ -0,0 +1,81 @@
+#define _GLIBCXX_DEBUG
+
+#include <bits/stdc++.h>
+typedef int64_t int64;
+typedef uint32_t uint;
+typedef uint64_t uint64;
+using namespace std;
+
+//---
+
+template <typename T>
+inline void print(const T& rhs){ std::cout<<" = "<<rhs<<std::endl; }
+template <typename T>
+inline void print(const std::vector<T>& rhs){
+    std::cout<<" = [ ";
+    for(uint i=0; i<rhs.size(); ++i){ std::cout<<rhs[i]<<' '; }
+    std::cout<<"]"<<std::endl;
+}
+template <typename T>
+inline void print(const std::vector<std::vector<T>>& rhs){
+    std::cout<<" = "<<std::endl;
+    std::cout<<"[[ ";
+    for(uint p=0; p<rhs.size(); ++p){
+        if(p!=0){ std::cout<<" [ "; }
+        for(uint q=0; q<rhs[p].size(); ++q){
+            std::cout<<rhs[p][q]<<' ';
+        }
+        if(p!=rhs.size()-1){ std::cout<<"]"<<std::endl; }
+    }
+    std::cout<<"]]"<<std::endl;
+}
+template <typename TL, typename TR>
+inline void print(const std::vector<std::pair<TR,TL>>& rhs){
+    std::cout<<" = [";
+    uint i=0;
+    for(; i<rhs.size()-1; ++i){ std::cout<<"[f: "<<rhs[i].first<<", s: "<<rhs[i].second<<"], "; }
+    std::cout<<"[f: "<<rhs[i].first<<", s: "<<rhs[i].second<<"]]" << std::endl;
+}
+#define printn(var) {printf("%s", #var);print(var);}
+#define printn_all(var) {printf("%s(%d): ", __func__, __LINE__);printf("%s", #var);print(var);}
+
+//---
+
+template <class T,
+          class T_alloc = std::allocator<T>,
+          class T_alloc_d = typename std::allocator_traits<T_alloc>::template rebind_alloc<double>
+          >
+class vec{
+    T* pT;
+    uint ttSize;
+    T_alloc_d aE_state; // current state of T_alloc_e allocation.
+    
+public:
+    vec(uint size){
+        ttSize = size;
+
+        pT = aE_state.allocate(ttSize); // memory allocation
+        for(uint i=0; i<ttSize; ++i){ aE_state.construct(&pT[i]); } // call coustructor
+    }
+    ~vec(){
+        for(uint i=0; i<ttSize; ++i){ aE_state.destroy(&pT[i]); } // call destructor
+        aE_state.deallocate(pT, ttSize); // release memory
+    }
+
+    T& operator[](uint i){ return pT[i]; }
+};
+
+int main(){
+    vec<double, std::allocator<char>> v(10);
+    
+    for(uint i=0; i<10; ++i){
+        v[i] = (double)i/10.0;
+    }
+    
+    for(uint i=0; i<10; ++i){
+        std::cout << (double)v[i] << std::endl;
+    }
+    
+}
+
+
