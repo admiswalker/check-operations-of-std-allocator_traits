ee793ee0532a1a5925b0aa0693a488b3f192cc53
admiswalker <admiswalker@gmail.com>
2020-07-07 00:09:05 +0900

    rm unused line

--- main_ex2.cpp
+++ main_ex2.cpp
@@ -40,171 +40,27 @@ inline void print(const std::vector<std::pair<TR,TL>>& rhs){
 
 //---
 
-/*
-https://stackoverflow.com/questions/33265165/does-stdallocator-traits-define-rebind-alloc-if-rebind-not-present-in-custom-a
-
-template<typename T>
-using RebindAlloc =
-  typename std::allocator_traits<MyAllocator<void>>::template rebind_alloc<T>;
-
-*/
-
-/*
-struct AB{
-private:
+template <class T, class T_alloc_e>
+class vec{
+    T* pT;
+    uint ttSize;
+    T_alloc_e aE_state; // current state of T_alloc_e allocation.
+    
 public:
-    AB(){}
-    ~AB(){}
-    
-    int A;
-    int B;
-};
+    vec(uint size){
+        ttSize = size;
 
-
-//template <class T_alloc_e=std::allocator_traits<struct AB>> class example_ac;
-
-template <class T_alloc_e>
-class example_ac{
- private:
-    int ttSize;
-    struct AB* pT;
-    
- public:
-    example_ac(){
-        printf("in const\n");
-        ttSize      = 100;
-        
-        auto alc_e = T_alloc_e();
-        pT         = alc_e.allocate(ttSize);
+        pT = aE_state.allocate(ttSize); // memory allocation
+        for(uint i=0; i<ttSize; ++i){ aE_state.construct(&pT[i]); } // call coustructor
     }
-    ~example_ac(){
-        printf("in dst\n");
+    ~vec(){
+        for(uint i=0; i<ttSize; ++i){ aE_state.destroy(&pT[i]); } // call destructor
+        aE_state.deallocate(pT, ttSize); // release memory
     }
 };
-
 
 int main(){
-    //--------------------------------------------------------------------
-    // ref: https://cpprefjp.github.io/reference/memory/allocator.html
-
-    auto alc = std::allocator<int>();
-
-    // 10要素のint型が入る領域を確保
-    int* arr = alc.allocate(10);
-
-    // 確保した領域の各要素を構築する(コンストラクタを呼び出す)
-    for (std::size_t i = 0; i != 10; ++i) alc.construct(arr + i);
-
-    std::iota(arr, arr + 10, 0);
-    std::for_each(arr, arr + 10, [](int i) { std::cout << i << " "; });
-    std::cout << std::endl;
-
-    // 配列の各要素を破棄する(デストラクタを呼び出す)
-    for (std::size_t i = 0; i != 10; ++i) alc.destroy(arr + i);
-
-    // 領域を解放する
-    alc.deallocate(arr, 10);
-
-    //--------------------------------------------------------------------
-    
-    using RebindAlloc = std::allocator_traits<std::allocator<int>>::template rebind_alloc<std::pair<int,int>>;
-
-    auto ralc = RebindAlloc();
-    std::pair<int,int>* pPair = ralc.allocate(10);
-    for(uint i=0; i<10; ++i){
-        pPair[i].first = i;
-        pPair[i].second = i*10;
-    }
-
-    for(uint i=0; i<10; ++i){
-        std::cout << pPair[i].first << ", " << pPair[i].second << std::endl;
-    }
-    
-    ralc.deallocate(pPair, 10);
-    
-    //--------------------------------------------------------------------
-    
-    example_ac<std::allocator_traits<struct AB>> ex;
-    
-    return 0;
-}
-*/
-
-
-/*
-// ref: https://cpprefjp.github.io/reference/memory/allocator_traits.html
-template <class T, class T_alloc_e>
-class MyVector{
-    T* pT;
-    std::size_t ttSize;
-    T_alloc_e aE_state; // current state of T_alloc_e allocation.
-    
-public:
-    MyVector(std::size_t size){
-        ttSize = size;
-
-        // メモリを確保
-        pT = std::allocator_traits<T_alloc_e>::allocate(aE_state, ttSize);
-
-        // 要素を構築
-        for(std::size_t i=0; i<ttSize; ++i) {
-            std::allocator_traits<T_alloc_e>::construct(aE_state, &pT[i]);
-        }
-    }
-
-    ~MyVector(){
-        // 要素を破棄
-        for(std::size_t i=0; i<ttSize; ++i) {
-            std::allocator_traits<T_alloc_e>::destroy(aE_state, &pT[i]);
-        }
-
-        // メモリを解放
-        std::allocator_traits<T_alloc_e>::deallocate(aE_state, pT, ttSize);
-    }
-};
-
-int main()
-{
-    MyVector<int, std::allocator<int>> v(3);
-}
-*/
-
-
-template <class T, class T_alloc_e>
-class MyVector{
-    T* pT;
-    std::size_t ttSize;
-    T_alloc_e aE_state; // current state of T_alloc_e allocation.
-    
-public:
-    MyVector(std::size_t size){
-        ttSize = size;
-
-        aE_state = T_alloc_e();
-
-        // メモリを確保
-        pT = aE_state.allocate(ttSize);
-
-        // 要素を構築
-        for(std::size_t i=0; i<ttSize; ++i) {
-            aE_state.construct(&pT[i]);
-        }
-    }
-
-    ~MyVector(){
-        // 要素を破棄
-        for(std::size_t i=0; i<ttSize; ++i) {
-            aE_state.destroy(&pT[i]);
-        }
-
-        // メモリを解放
-        aE_state.deallocate(pT, ttSize);
-    }
-};
-
-int main()
-{
-    MyVector<int, std::allocator<int>> v(3);
+    vec<int, std::allocator<int>> v(3);
 }
 
 
